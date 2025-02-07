5eaf90b4a6ac42e586f4d9837f7f7c72bc8b8c20
daliamansour <daliamansour297@gmail.com>
2025-02-07 03:20:19 +0200

    gemStore v1.0.0

---
+++ lib/config/routes/routes.dart
@@ -0,0 +1,11 @@
+
+
+
+class Routes{
+  static const String onBoardingScreen='/onBoardingScreen';
+  static const String loginScreen='/loginScreen';
+  static const String homeScreen='/homeScreen';
+  static const String favouriteScreen='/searchScreen';
+  static const String categoryScreen='/fullproductScreen';
+
+}
