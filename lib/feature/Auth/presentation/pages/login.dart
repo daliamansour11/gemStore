3154948c6e94e53cc804b424522454fad5012441
heba <hebatarekg@gamil.com>
2025-03-15 21:49:48 +0200

    Added new updates

--- lib/feature/Auth/presentation/pages/login.dart
+++ lib/feature/Auth/presentation/pages/login.dart
@@ -133,20 +133,16 @@ class _LoginButton extends StatelessWidget {
 class _SocialLogin extends StatelessWidget {
   const _SocialLogin();
   Future signInWithGoogle() async {
-    // Trigger the authentication flow
     final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
 
-    // Obtain the auth details from the request
     final GoogleSignInAuthentication? googleAuth =
         await googleUser?.authentication;
 
-    // Create a new credential
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth?.accessToken,
       idToken: googleAuth?.idToken,
     );
 
-    // Once signed in, return the UserCredential
      await FirebaseAuth.instance.signInWithCredential(credential);
     // Navigator.of(context)
     //     .pushNamedAndRemoveUntil('HomeScreen', (route) => false);
